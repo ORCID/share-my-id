0ff96465dab46eb6cecb8178dcf34b791f3667e2
rcpeters <info@rcpeters.com>
2016-11-04 16:32:10 -0700

    make config easier

--- client-app.js
+++ client-app.js
@@ -29,7 +29,7 @@ secureServer = createServer({
   agreeTos: true, // Required for letsencrypt.
   debug: (config.AUTO_SNI_DEBUG === 'true'), // Add console messages and uses staging LetsEncrypt server. (Disable in production)
   domains: config.DOMAINS.split(','), // List of accepted domain names. (You can use nested arrays to register bundles with LE).
-  forceSSL: true, // Make this false to disable auto http->https redirects (default true).
+  forceSSL: (config.FORCE_SSL === 'true'), // Make this false to disable auto http->https redirects (default true).
   redirectCode: 301, // If forceSSL is true, decide if redirect should be 301 (permanent) or 302 (temporary). Defaults to 302
   ports: {
     http: parseInt(config.PORT_HTTP), // Optionally override the default http port.
