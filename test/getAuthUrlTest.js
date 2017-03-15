0993e7be0e3c174a1cf6ca56169b95c82eef2bff
rcpeters <info@rcpeters.com>
2017-03-14 20:22:49 -0700

    adding travis file

--- test/getAuthUrlTest.js
+++ test/getAuthUrlTest.js
@@ -4,7 +4,7 @@ var OcridOAuthUtil = require('../local_modules/orcid-oauth-util.js').OcridOAuthU
 
 var ooau = new OcridOAuthUtil('client_id_1','client_secrect_1','https://test.com/1','https://test.com/2')
 
-test('creates correct url',  t => {
+test('test creates correct url',  t => {
     t.is(
     	'https://test.com/1?redirect_uri=getAuthUrl&scope=%2Fauthenticate&response_type=code&client_id=client_id_1&show_login=true&state=state1',
     	ooau.getAuthUrl('getAuthUrl','state1')
