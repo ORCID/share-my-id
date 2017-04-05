e2b9c69ed64aba229a0c4ae0c62a4f22661f7ecd
Manuel Calvo <m.calvo@ost.orcid.org>
2017-04-05 15:57:16 -0600

    Controller for error page

--- src/app/add-id-error/add-id-error.component.ts
+++ src/app/add-id-error/add-id-error.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-id-error.component.scss']
 })
 export class AddIdErrorComponent implements OnInit {
+    publicKey: string;
 
-  constructor() { }
+    collections: Collection[];
 
-  ngOnInit() {
-  }
+    constructor(
+        private collectionService: CollectionService
+    ) {
+        this.publicKey = this.getParameterByName('state'); //name orcid
+    }
+
+    private getParameterByName(name:string): string {
+        var url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) {
+            return null;
+        }
+        if (!results[2]) {
+            return '';
+        } 
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
+    getCollections(): void {
+        this.collectionService.getCollection(this.publicKey).subscribe( 
+            collections => {
+                this.collections = collections;
+            }
+        );
+    }    
+
+    ngOnInit() {
+        this.getCollections();
+    }
 
 }
