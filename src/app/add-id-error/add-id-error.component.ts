6b9ad3866472dac339b14d54c17fb84968993787
Manuel Calvo <m.calvo@ost.orcid.org>
2017-04-05 16:14:44 -0600

    Updating error markup and redirecting correctly to the add id page

--- src/app/add-id-error/add-id-error.component.ts
+++ src/app/add-id-error/add-id-error.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/switchMap';
 
+import { CollectionService } from './../shared/collection/collection.service';
+
 @Component({
   selector: 'app-add-id-error',
   templateUrl: './add-id-error.component.html',
@@ -15,7 +17,25 @@ export class AddIdErrorComponent implements OnInit {
     private sub: any;
     private publicKey: string;
 
-    constructor(private route: ActivatedRoute) { }
+    constructor(
+        private collectionService: CollectionService,
+        private route: ActivatedRoute
+    ) { }
+
+    authenticate(): void {
+        // make sure the user is logged out before sending them over
+        
+        this.collectionService.logUserOut().subscribe(
+            response => { 
+                window.location.href  = '/add-id-authorize/' + this.publicKey;
+            }, 
+            err => { 
+                // ignore error
+                window.location.href  = '/add-id-authorize/' + this.publicKey;      
+            }
+        );
+        
+    }
 
     ngOnInit() {
         this.sub = this.route.params.subscribe(
