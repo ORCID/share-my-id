149a6a6035ad14d3c395ad5f164e8a842b77f3dc
rcpeters <info@rcpeters.com>
2017-04-07 18:56:16 -0700

    move logout and authenicate redirects into util

--- src/app/shared/orcid-util/orcid-util.service.ts
+++ src/app/shared/orcid-util/orcid-util.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+
 import { ConfigService } from './../../shared/config/config.service';
 
-import { Jsonp } from '@angular/http';
+import { Jsonp, Response} from '@angular/http';
 
 
 @Injectable()
 export class OrcidUtilService {
 
-  constructor(
-  	private jsonp: Jsonp,
-    private configService: ConfigService
-) { }
+    constructor(
+        private jsonp: Jsonp,
+        private configService: ConfigService
+    ) {}
 
-    logUserOut() {
+    private logUserOutObserable(): Observable<Response> {
         /* 
            BROKEN_CALLBACK_IS_FINE_SEE_CODE should be JSONP_CALLBACK
            https://medium.com/@FanggengYang/how-to-use-angular-2-jsonp-c75460ad614c
@@ -24,5 +26,26 @@ export class OrcidUtilService {
               config => 
                 this.jsonp.get(config['ORCID_URL'] + '/userStatus.json?logUserOut=true&callback=BROKEN_CALLBACK_IS_FINE_SEE_CODE'))
     }
+
+    logUserOut(): void {
+        this.logUserOutObserable().subscribe(
+            response => { /* do nothing */ },
+            err => { /* ignore error */ }
+        );
+    }
+
+    createAuth(): void {
+        this.logUserOutObserable().subscribe(
+            response => { window.location.href  = '/create-smid-authorize'; },
+            err => { window.location.href  = '/create-smid-authorize'; }
+        );
+	}
+
+    addIdAuth(pubKey: string): void {
+	        this.logUserOutObserable().subscribe(
+            response => { window.location.href  = '/add-id-authorize/' + pubKey; },
+            err => { window.location.href  = '/add-id-authorize/' + pubKey; }
+        );
+	}
 
 }
