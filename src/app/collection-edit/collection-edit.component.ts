40049e676aca4db064d501647e46b8fd81f4699c
Liz Krznarich <ejkrznarich@gmail.com>
2017-04-12 17:40:54 -0500

    validate email before sending to smid manager

--- src/app/collection-edit/collection-edit.component.ts
+++ src/app/collection-edit/collection-edit.component.ts
@@ -106,24 +106,31 @@ export class CollectionEditComponent implements OnInit, OnDestroy {
     }
 
     submitEmail( form: any ): void {
-        var form_string = JSON.stringify(form);
-        console.log("Email string: " + form_string);
         this.formEmailSubmitted = true;
-        this.collectionService.editEmail( form_string, this.publicKey, this.privateKey ).subscribe(
-            (response) => {
-                this.response = response;
-                console.log(this.response);
-                this.showEmailErrorMessage = false;
-                this.showEmailSuccessMessage = true; // <- Update to change the status on the ajax call result
-            },
-            (err) => {
-                console.log(err);
-                this.showEmailErrorMessage = true;
-                this.showEmailSuccessMessage = false;
-        
-            },
-            () => {}
-        );
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (re.test(form.email) == false) {
+            console.log("invalid email");
+            this.showEmailErrorMessage = true;
+            this.showEmailSuccessMessage = false;
+        } else {
+            var formString = JSON.stringify(form);
+            console.log("Email string: " + formString);
+            this.collectionService.editEmail( formString, this.publicKey, this.privateKey ).subscribe(
+                (response) => {
+                    this.response = response;
+                    console.log(this.response);
+                    this.showEmailErrorMessage = false;
+                    this.showEmailSuccessMessage = true; // <- Update to change the status on the ajax call result
+                },
+                (err) => {
+                    console.log(err);
+                    this.showEmailErrorMessage = true;
+                    this.showEmailSuccessMessage = false;
+            
+                },
+                () => {}
+            );
+        }
     }
 
     submitForm( form: any ): void {
