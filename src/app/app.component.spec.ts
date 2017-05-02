d1707fc3490cbb841c6c433f51a4269dc58e02fa
rcpeters <info@rcpeters.com>
2017-05-02 13:46:05 -0700

    revert back

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Test break! Share My ORCID iD');
+    expect(compiled.querySelector('h1').textContent).toContain(' Share My ORCID iD');
   }));
 });
